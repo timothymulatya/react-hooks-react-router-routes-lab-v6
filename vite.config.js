7f344e81b2f2abd209d46a8c629264a6e27510c0
timothymulatya <timothymulatya@gmail.com>
2025-11-22 13:16:34 +0300

    setup.js vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/__tests__/setup.js",
-  },
-});
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.js',
+    transformMode: {
+      web: [/\.[jt]sx?$/]
+    }
+  }
+})
