bffa179c8eaccf975b5400731c7e4f1675f8f62f
timothymulatya <timothymulatya@gmail.com>
2025-11-22 13:23:08 +0300

    jsx renaming

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,7 +1,7 @@
 // src/__tests__/setup.js
-import { afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
 
 afterEach(() => {
   cleanup();
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/__tests__/setup.js',
-    transformMode: {
-      web: [/\.[jt]sx?$/]
-    }
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.js",
+  },
+});
