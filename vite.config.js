07e63b21482d19f104a4dfcb4ccb97e1149fe9fe
timothymulatya <timothymulatya@gmail.com>
2025-11-22 13:09:38 +0300

    vite updates

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
     globals: true,
-    setupFiles: './src/__tests__/setup.js',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.js",
+  },
+});
